7fcd8a7730da4568deb4712cbd11221227a1b86a
nosthertus <nosthertus@gmail.com>
2017-07-07 04:27:02 -0500

    + Mongoose plugin file
    *Added plugin file for mongoose schema

---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = function(schema, options){
+	/**
+	 * Compiles a object map of the properties that matches with the scope list
+	 * 
+	 * @param  {String} scope The name of the scope to match with the properties
+	 * @return {Object}       The map of fields to apply in the field selection
+	 */
+	function getScopedFields(scope){
+		var scoped = {};
+		var opts   = schema.options;
+
+		if(typeof opts.scopes !== "undefined"){
+			for(p in schema.tree){
+				if(_.inArray(opts.scopes[scope], p)){
+					scoped[p] = true;
+				}
+			}
+		}
+
+		return scoped;
+	}
+
+	/**
+	 * Add the query scope into the schema function
+	 * this should do a normal find with the compiled selected fields
+	 * 
+	 * @param  {String} scope    The name of the scope to apply in the fields
+	 * @param  {Object} criteria The criteria to set in the search
+	 * @return {Query}           The query result of the search
+	 */
+	schema.static("findByScope", function(scope, criteria = {}){
+		return this.find(criteria, getScopedFields(scope));
+	});
+};
